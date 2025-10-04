6a57949f0c00d3d8a98258047f21cbbc047d2b75
RodRickSia <doquocduy698@gmail.com>
2025-10-04 14:13:31 +0700

    Feat: Create Fastapi services for embedding. Need to manage routing for fast next

--- backend/src/embedding_services/document_embedder.py
+++ backend/src/embedding_services/document_embedder.py
@@ -1,36 +1,49 @@
+# ...existing code...
 import requests
 from langchain_community.embeddings import JinaEmbeddings
-from dotenv import load_dotenv
-import os
+from functools import lru_cache
+import asyncio
 from pydantic import SecretStr
-import requests
-MODEL_NAME = "jina-embeddings-v4"
-load_dotenv()
-_session = requests.Session()
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List, Any
 
 
-# Safety first
-_doc_embedder = os.getenv("API_DOCUMENT_EMBEDDING")
-if not _doc_embedder:
-    raise ValueError("API_DOCUMENT_EMBEDDING environment variable is not set")
-DOC_EMBEDDER: SecretStr = SecretStr(_doc_embedder)
 
-_query_embedder = os.getenv("QUERY_EMBEDDING")
-if not _query_embedder:
-    raise ValueError("QUERY_EMBEDDING environment variable is not set")
-QUERY_EMBEDDER: SecretStr = SecretStr(_query_embedder)
+class Settings(BaseSettings):
+    
+    # BaseSetting autoload from env vars
+    api_document_embedding: SecretStr = SecretStr("") # This is to make pylance stfu about missing constructor arg  
+    api_query_embedding: SecretStr = SecretStr("")
+    model_name: str = "jina-embeddings-v4"
+    
+    # env_file is path to the env file it does not look for env in parent dirs
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-
-async def embed_document(text: str):
-    doc_embedder = JinaEmbeddings(
-        jina_api_key=DOC_EMBEDDER, model_name=MODEL_NAME, session=_session
-    )
-    return await doc_embedder.aembed_documents([text])
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
 
 
-async def embed_query(text: str):
-    query_embedder = JinaEmbeddings(
-        jina_api_key=QUERY_EMBEDDER, model_name=MODEL_NAME, session=_session
-    )
-    return await query_embedder.aembed_query(text)
+class EmbeddingClient():
+    def __init__(
+        self,
+        api_key: SecretStr,
+        model_name: str = "jina-embeddings-v4",
+    ):
+        self.__api_key = api_key
+        self.model_name = model_name
+        self.session = requests.Session()
+        self.embedder = JinaEmbeddings(
+            jina_api_key=self.__api_key, model_name=self.model_name, session=self.session
+        )
 
+    def close(self) -> None:
+        self.session.close()
+    
+    
+    # aembed is truly async function in Langchain
+    async def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        return await self.embedder.aembed_documents(texts)
+    
+    async def embed_query(self, text: str) -> List[float]:
+        return await self.embedder.aembed_query(text)
